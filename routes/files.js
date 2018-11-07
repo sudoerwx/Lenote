8d76a3b86adfc48126e4a7ca990322d706ea18a1
Nazar Voronych <voronych.nazik@gmail.com>
2018-11-07 10:47:19 +0200

    change users api

--- routes/files.js
+++ routes/files.js
@@ -4,8 +4,8 @@ const fs = require('fs');
 const fse = require('fs-extra');
 const path = process.cwd();
 
-router.get('/:id/:file', function(req, res) {
-    fs.readFile(`${path}/userfiles/${req.params.id}/${req.params.file}.md`,"utf8", function (err, data) {
+router.get('/:file', function(req, res) {
+    fs.readFile(`${path}/userfiles/${res.user._id}/${req.params.file}.md`,"utf8", function (err, data) {
         if (err)
             res.sendStatus(404);
         if (data)
@@ -13,20 +13,20 @@ router.get('/:id/:file', function(req, res) {
     });
 });
 
-router.post('/:id/:file', function(req, res) {
-    fse.ensureDir(`${path}/userfiles/${req.params.id}/`, err => {
+router.post('/:file', function(req, res) {
+    fse.ensureDir(`${path}/userfiles/${res.user._id}/`, err => {
         console.log(err) // => null
         // dir has now been created
     });
-    fs.writeFile(`${path}/userfiles/${req.params.id}/${req.params.file}.md`, req.body, function (err) {
+    fs.writeFile(`${path}/userfiles/${res.user._id}/${req.params.file}.md`, req.body, function (err) {
         if (err) res.sendStatus(500);
         if(!err) res.sendStatus(200);
         console.log(req.body);
     });
 });
 
-router.delete('/:id/:file', function(req, res) {
-    fs.unlink(`${path}/userfiles/${req.params.id}/${req.params.file}.md`, function (err) {
+router.delete('/:file', function(req, res) {
+    fs.unlink(`${path}/userfiles/${res.user._id}/${req.params.file}.md`, function (err) {
         if (err) res.sendStatus(404);
         if(!err) res.sendStatus(200);
     });
