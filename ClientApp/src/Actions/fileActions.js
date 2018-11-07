136702230e940e6beb40fcc70c1c1483f9c08bc5
Ironimus <ironimus13@gmail.com>
2018-11-07 15:30:50 +0200

    some fixes

--- ClientApp/src/Actions/fileActions.js
+++ ClientApp/src/Actions/fileActions.js
@@ -1,9 +1,15 @@
 const sendFile = () => ({ type: "SEND_FILE" })
+const receiveFile = text => ({ type: "RECEIVE_FILE", payload: text })
 
 export const syncFile = getText => dispatch => {
     dispatch(sendFile)
-    fetch(`/files/testUser/testFile`, {
+    fetch(`/files/testFile`, {
         method: "POST",
         body: getText()
     })
+}
+
+export const getFile = id => async dispatch => {
+  const text = await fetch(`/files/${id}`)
+  dispatch(receiveFile(text))
 }
