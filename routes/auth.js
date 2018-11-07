82f5667a96adeaa75ef978a70c23cd7f63c2ad7c
Nazar Voronych <voronych.nazik@gmail.com>
2018-11-08 00:19:16 +0200

    add eslint

--- routes/auth.js
+++ routes/auth.js
@@ -1,21 +1,20 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const db = require("../database/db");
-const passport = require("passport");
+const passport = require('passport');
 
-//google login landler
+// google login landler
 router.get(
-	"/google",
-	passport.authenticate("google", { scope: ["profile", "email"] })
+    '/google',
+    passport.authenticate('google', {scope: ['profile', 'email']})
 );
-//google login callback
+// google login callback
 router.get(
-	"/google/callback",
-	passport.authenticate("google", { failureRedirect: "/login" }),
-	function(req, res) {
-		// Successful authentication, redirect home.
-		res.redirect("//localhost:3000");
-	}
+    '/google/callback',
+    passport.authenticate('google', {failureRedirect: '/login'}),
+    function(req, res) {
+      // Successful authentication, redirect home.
+      res.redirect('//localhost:3000');
+    }
 );
 
 module.exports = router;
--- sockets/editFileSocket.js
+++ sockets/editFileSocket.js
@@ -1,12 +1,12 @@
-const expressSession = require("express-session");
-const keys = require("../config/keys");
-const passportSocketIo = require("passport.socketio");
+const expressSession = require('express-session');
+const keys = require('../config/keys');
+
 exports = module.exports = function(io) {
-  var sessionMiddleware = expressSession({
+  const sessionMiddleware = expressSession({
     secret: keys.session.cookieKey,
-    store: new (require("connect-mongo")(expressSession))({
-      url: keys.mongodb.dbURI
-    })
+    store: new (require('connect-mongo')(expressSession))({
+      url: keys.mongodb.dbURI,
+    }),
   });
 
   io.use(function(socket, next) {
@@ -14,17 +14,17 @@ exports = module.exports = function(io) {
     sessionMiddleware(socket.request, {}, next);
   });
 
-  io.on("connection", function(socket) {
-    console.log("a user connected", socket.request.user);
+  io.on('connection', function(socket) {
+    console.log('a user connected', socket.request.user);
 
-    socket.on("patchText", function(patch) {
-      //send patch to another users
-      socket.broadcast.emit("user connected");
-      //patching file
+    socket.on('patchText', function(patch) {
+      // send patch to another users
+      socket.broadcast.emit('user connected');
+      // patching file
     });
 
-    socket.on("disconnect", function() {
-      console.log("user disconnected");
+    socket.on('disconnect', function() {
+      console.log('user disconnected');
     });
   });
 };
